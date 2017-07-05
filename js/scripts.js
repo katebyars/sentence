83b7be4e421d7318b9ce79ce756418176a7f9d18
Esti Shay and Kate Byars <kate@>
2017-07-05 14:23:20 -0700

    add form functionality with jQuery plus Bootstrap styling.

--- js/scripts.js
+++ js/scripts.js
@@ -4,15 +4,21 @@ $(document).ready(function() {
     var person2Input = $("input#person2").val();
     var animalInput= $("input#animal").val();
     var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
+    var fearExclamationInput = $("input#fear_exclamation").val();
+    var adjInput = $("input#adjective").val();
     var nounInput = $("input#noun").val();
+    var otherNounInput = $("input#other_noun").val();
+    var weaponInput = $("input#weapon").val();
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
     $(".animal").text(animalInput);
     $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
+    $(".fear_exclamation").text(fearExclamationInput);
+    $(".adjective").text(adjInput);
     $(".noun").text(nounInput);
+    $(".other_noun").text(otherNounInput);
+    $(".weapon").text(weaponInput);
 
     $("#story").show();
 
