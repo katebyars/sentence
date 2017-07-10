42a2558236f38b297f541e4a859e2bb046002592
Evan Johnson and Kate Byars <kate@>
2017-07-10 14:01:08 -0700

    take words longer than 3 and spit new string in reversed order

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,15 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var fearExclamationInput = $("input#fear_exclamation").val();
-    var adjInput = $("input#adjective").val();
-    var nounInput = $("input#noun").val();
-    var otherNounInput = $("input#other_noun").val();
-    var weaponInput = $("input#weapon").val();
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".fear_exclamation").text(fearExclamationInput);
-    $(".adjective").text(adjInput);
-    $(".noun").text(nounInput);
-    $(".other_noun").text(otherNounInput);
-    $(".weapon").text(weaponInput);
-
-    $("#story").show();
-
-    event.preventDefault();
+  $("form").submit(function(event) {
+    var userInput = $("#userInput").val().split(" ");
+    var overThree = userInput.map(function(word) {
+      if (word.length>=3) {
+        return word;
+        }
+    });
+      var reverse = overThree.reverse();
+      var join = reverse.join(" ");
+      alert(join);
+      event.preventDefault();
+    });
   });
-});
